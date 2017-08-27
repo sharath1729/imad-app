c8948259580387aa939748afbf726d94ef08b8d9
sharath1729 <30920018+sharath1729@users.noreply.github.com>
2017-08-27 16:50:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='New value';
+// move the image
+var img = document.getElementById('img');
+img.onclick = function(){
+    img.style.marginleft ='100px';
+}
