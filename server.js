d6e0e6d0d33642453315285c55e2d9288e0a8da4
sharath1729 <30920018+sharath1729@users.noreply.github.com>
2017-08-28 19:08:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,11 +50,7 @@ var articles= {
                         </p>`
             }
 };
-function createTemplate (data) {
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.content;
+
 var htmlTemplate=`<html>
     <head>
         <title>
