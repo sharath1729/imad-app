f943be65a32f4818b140c931a5d11ec74a2582ea
sharath1729 <30920018+sharath1729@users.noreply.github.com>
2017-08-28 19:02:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,11 @@ app.get('/:articlename',function (req,res) {
    var articlename= req.params.articlename;
    res.send(createTemplate(articles[articlename]));
 });
+var couter =0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
