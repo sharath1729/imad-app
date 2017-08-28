ba8a576c70cdc7d58dfd484ac7eebfd65e74d0e3
sharath1729 <30920018+sharath1729@users.noreply.github.com>
2017-08-28 18:54:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,1 @@
 console.log('Loaded!');
-var element = document.getElementById('main-text');
-
-// move the image
-
-
-var img = document.getElementById('madi');
-var marginleft=0;
-
-
-            function moveRight()  {
-            marginLeft =marginLeft + 10;
-            img.style.marginLeft=marginLeft + 'px';
-                }
-                            img.onclick = function(){
-                                var interval = setInterval(moveRight,50);
-                            };
